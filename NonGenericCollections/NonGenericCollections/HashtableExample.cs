b297769806d2f833871f2b948cafb0d1fe4fcdad
bapuji <bapujinayak999@gmail.com>
2023-11-09 11:20:16 +0530

    NOn-Generic collection completed

--- NonGenericCollections/NonGenericCollections/HashtableExample.cs
+++ NonGenericCollections/NonGenericCollections/HashtableExample.cs
@@ -14,11 +14,37 @@ namespace NonGenericCollections
                 Console.WriteLine($"Colour Name is {colour.Key} and colour hashcode is {colour.Value}");
             }
 
+            //Propertys
+
+            //Count
+            int count = colours.Count;
+
+            //Item Property
+            object value = colours["Red"];
+
+            //Keys Property
+            ICollection keys = colours.Keys;
+
+            //Values Property
+            ICollection values = colours.Values;
+
+            //Methods
+
+            //Clone()
+            Hashtable copy = (Hashtable)colours.Clone();
+
+            //ContainsKey(key)
+            bool containsKey = colours.ContainsKey("Red");
+
+            //ContainsValue(value)
+            bool containsValue = colours.ContainsValue("#FF0000");
+
             //Remove()
             colours.Remove("Red");
 
             //Clear
             colours.Clear();
+
         }
     }
 }
