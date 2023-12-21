9ce5bd7906dfec0b845e831d2b4f17f29375ba35
Bapuji <bapuji.nayak@interbizconsulting.com>
2023-12-21 11:51:18 +0530

    Project 6 added

---
+++ Project6/Project6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Project6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome To Custom Weather Client Tool");
+                string filePath = @"C:\Users\bapuj\OneDrive\Desktop\BapujiPrac\Project6\Project6\CityConstants.json";
+                string jsonString = System.IO.File.ReadAllText(filePath);
+                if (jsonString != null)
+                {
+                    List<City> cities = JsonSerializer.Deserialize<List<City>>(jsonString);
+                    Starting.Start(cities);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.InnerException?.Message);
+            }
+        }
+    }
+}
---
+++ Project6/Project6/Starting.cs
@@ -0,0 +1,30 @@
+﻿namespace Project6
+{
+    class Starting
+    {
+        public static void Start(List<City> cities)
+        {
+            try
+            {
+                Console.WriteLine("Please Enter the city for the weather related information.");
+                string cityName = Console.ReadLine();
+                if (cityName == null || cityName == "")
+                {
+                    Console.WriteLine("City cannot be Empty.");
+                    return;
+                }
+                var city = cities.Find(x => x.city == cityName);
+                if (city == null)
+                {
+                    Console.WriteLine("Weather related information is not available for the enterd city.");
+                    return;
+                }
+                ApiCall.GetWetherData(city).Wait();
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message, ex.InnerException?.Message);
+            }
+        }
+    }
+}
